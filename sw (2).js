53d98ab0135eccc0712dcf17ad2153a06c618271
MicroTools-Hub <malkapurwalahuzeifa123@gmail.com>
2026-02-02 19:59:45 +0530

    MULTITAG FIX MAYBE

--- sw (2).js
+++ sw (2).js
@@ -1,7 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10556649,
-    "zoneIds": [10548835, 10556649]
+    "zoneId": 10556649
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
